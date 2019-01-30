d1a4b1224c605250f062c71a10b44cbe1a693321
miriamvj <miriamvicentejimenez@gmail.com>
2019-01-30 17:49:39 +0100

    the counting sequence

---
+++ session2/dna_count_file.py
@@ -0,0 +1,22 @@
+def count(filename):
+    with open (filename, 'r') as f:
+        acounter = 0
+        gcounter = 0
+        tcounter = 0
+        ccounter = 0
+        for line in f:
+            if '>' in line:
+                line = line.replace(line, ' ')
+            else:
+                for elem in line:
+                    if elem == 'A':
+                        acounter += 1
+                    elif elem == 'C':
+                        ccounter += 1
+                    elif elem == 'T':
+                        tcounter += 1
+                    elif elem == 'G':
+                        gcounter += 1
+    return acounter, gcounter, tcounter, ccounter
+print('The order for the counting is: A, G, T, C:',count('FRAT1-gene'))
+
