058c2ec61a17cb909c470535d6e58d10592bf1d3
miriamvj <miriamvicentejimenez@gmail.com>
2019-02-06 17:03:54 +0100

    Exercise from session 5

---
+++ session-5/Seq1.py
@@ -0,0 +1,33 @@
+class Seq:
+    """A class for representing sequences"""
+    def __init__(self, strbases):
+        print('New sequence created!')
+
+        self.strbases = strbases
+
+    def len(self):
+        return len(self.strbases)
+        # Here we have created the variable, and we can call it at any time from the outside
+
+class Gene(Seq):
+    """This class is derived from the Seq
+        All the onjects of class Gene will
+        inheritage the methods from Seq Class"""
+    pass
+
+s1 = Gene(Seq('ATTCGATCC'))
+s2 = Seq('AAAGG')
+
+str1 = s1.strbases
+str2 = s2.strbases
+
+l1 = s1.len()
+l2= s2.len()
+
+
+print('Sequence 1:{}'.format(str1))
+print('length:{}'.format(l1))
+print('Sequence 2:{}'.format(str2))
+print('length:{}'.format(l2))
+
+print('the end')
