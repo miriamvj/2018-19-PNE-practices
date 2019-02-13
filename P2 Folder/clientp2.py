305adfe39440b26d435442065181d92104f2cc97
miriamvj <miriamvicentejimenez@gmail.com>
2019-02-13 18:31:19 +0100

    Practice 2 Extra

--- P2 Folder/clientp2.py
+++ P2 Folder/clientp2.py
@@ -4,22 +4,16 @@ import socket
 while True:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print('Socket created')
-    PORT = 8083
+    PORT = 8085
     IP = "212.128.253.80"
     s.connect((IP, PORT))
-
     s1 = input('Please, enter a sequence:')
-    from Seq import Seq
-    s1 = Seq(s1)
-    s2 = Seq(s1.reverse())
-    s3 = Seq(s2.complement())
-    s.send(str.encode(s3.strbases))
+
+    s.send(str.encode(s1))
 
     msg = s.recv(2048).decode('utf-8')
     print('Message from the server')
     print(msg)
-    print(s1.strbases)
-    print(s2.strbases)
-    print(s3.strbases)
+
 
     s.close()
