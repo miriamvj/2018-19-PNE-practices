377d6570bdcfe596d17f72bca4569c1ac3d6d683
miriamvj <miriamvicentejimenez@gmail.com>
2019-03-05 16:17:27 +0100

    Webserver File for session 13

---
+++ session-13/webserver1.py
@@ -0,0 +1,30 @@
+import http.server
+import socketserver
+
+PORT = 8003
+
+# Request line rpints the first line
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        print('GET received')
+
+        print('Request line:' + self.requestline)
+        print(' Cmd:' + self.command)
+        print(' Path:' + self.path)
+
+        content = 'I  am your passed in Algebra!.'
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/plain')
+        self.send_header('Content.Length', len(str.encode(content)))
+        self.end_headers()
+
+        self.wfile.write(str.encode(content))
+        return
+
+
+Handler = TestHandler
+
+with socketserver.TCPServer(('', PORT), Handler) as httpd:
+    print('Serving at PORT', PORT)
+    httpd.serve_forever()
